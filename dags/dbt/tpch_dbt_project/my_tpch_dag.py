2efb06132e3b515838e809140120e3bc7ec01510
admin <admin@Livinglys-MacBook-Pro-2.local>
2023-08-21 22:12:28 -0400

    Added_arflow_variable_code_for_more_control_over_the_dag

--- dags/dbt/tpch_dbt_project/my_tpch_dag.py
+++ dags/dbt/tpch_dbt_project/my_tpch_dag.py
@@ -4,11 +4,15 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
+from airflow.models import Variable
 
+# DAG Variable
+dag_var = Variable.get("tpch_first_dag",deserialize_json=True, default_var=None)
 
-dag = DAG('run_first_dbt_project', description='DBT Execution Mode DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2023, 6, 12), catchup=False)
+dag = DAG(dag_var.get('dag_id'),
+            description=dag_var.get('description'),
+            schedule_interval=dag_var.get('schedule_interval'),
+            start_date=datetime(2023, 6, 12), catchup=False)
 
 # Variables
 DBT_PROJECT_PATH = f"{os.environ['AIRFLOW_HOME']}dags/dbt/tpch_dbt_project"
